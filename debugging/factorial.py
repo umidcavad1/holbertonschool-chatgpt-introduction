fc074cebfe8c94402501d5367236a0c6c5b6fc04
umidcavad1 <umidcavadov088@gmail.com>
2026-04-03 17:20:32 +0000

    task 0

---
+++ debugging/factorial.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1   # fix: decrease n
+    return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
+
